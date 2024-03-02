a6dc83d74d6637047fec6d5fee82bd4b0c50914b
Patrik <psirb@myuax.com>
2024-03-02 14:30:58 +0100

    Clase DadoIA completo y comentado, falta el main

--- src/Ejercicio1/DadoIA.java
+++ src/Ejercicio1/DadoIA.java
@@ -1,8 +1,45 @@
 package Ejercicio1;
-import java.util.*:
+import java.util.*;
 public class DadoIA {
     int caras;
     private Random random;
     private HashMap<Integer, Integer> historialResultados;
+
+    public DadoIA(int caras) {  //Creamos un constructor que recibe el numero de caras del dado
+        this.caras = caras;
+        random = new Random();
+        this.historialResultados = new HashMap<>(); //Creamos un HashMap para guardar el historial de resultados
+        for (int i = 1; i <= caras; i++) {  //Inicializamos el historial de resultados con 0
+            this.historialResultados.put(i, 0);
+        }
+    }
+
+
+    public int lanzarDado () throws IllegalArgumentException {  //Creamos un metodo que simula el lanzamiento del dado donde las caras solo pueden estar entre 1 y 6.
+        int resultado = random.nextInt(caras) + 1;  //Generamos un numero aleatorio entre 1 y el numero de caras del dado
+        return resultado;
+    }
+
+    public void actualizarHistorial(int resultado) {
+        historialResultados.put(resultado, historialResultados.get(resultado) + 1);
+    }
+
+    public double predecir(int cara){
+        int totalLanzamientos = 0;
+        for(Integer valor : historialResultados.values()){
+            totalLanzamientos += valor;
+        }
+        if(totalLanzamientos > 0){
+            return (double)historialResultados.get(cara) / totalLanzamientos;
+        } else {
+            return 0;
+        }
+    }
+
+    public void mostrarResultados(){
+        for (int i = 1; i <= caras; i++) {
+            System.out.println("Cara " + i + ": " + historialResultados.get(i));
+        }
+    }
 
 }
--- src/Ejercicio1/SimuladorDados.java
+++ src/Ejercicio1/SimuladorDados.java
@@ -4,6 +4,7 @@ package Ejercicio1;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class SimuladorDados {
     public static void main(String[] args) {
-
+        DadoIA dado1 = new DadoIA(6);
+        DadoIA dado2 = new DadoIA(10);
     }
 }
